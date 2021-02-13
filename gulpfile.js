ce04b481bb2a4adad3336d1409a70bd3e58b0641
nizami <nizami.dev@gmail.com>
2021-02-13 06:25:29 +0300

    Update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
-const { src, dest, series } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const rename = require('gulp-rename');
 const through = require('through2');
 const marked = require('marked');
 const del = require('delete');
+const fs = require('fs');
+const mustache = require('mustache');
+
+const layoutTemplate = fs.readFileSync('./src/layout.html').toString();
+const postsGlob = 'src/**/*.md';
 
 function clean(cb) {
   del(['dist'], cb);
 }
 
 function build() {
-  return src('src/*.md')
+  return src(postsGlob)
     .pipe(
       through.obj(function (file, _, cb) {
         if (file.isBuffer()) {
-          const html = marked(file.contents.toString());
-          file.contents = Buffer.from(html);
+          const view = {
+            content: marked(file.contents.toString()),
+          };
+          const output = mustache.render(layoutTemplate, view);
+          file.contents = Buffer.from(output);
         }
         cb(null, file);
       })
@@ -23,4 +31,6 @@ function build() {
     .pipe(dest('dist'));
 }
 
-exports.default = series(clean, build);
+exports.default = function () {
+  watch(postsGlob, { ignoreInitial: false }, series(clean, build));
+};
