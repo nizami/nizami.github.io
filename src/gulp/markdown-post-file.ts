f155bf39cd9c436bfc0fa0bab7b2b4c32285b366
nizami <nizami.dev@gmail.com>
2021-02-15 05:11:30 +0300

    Remove vinyl package

--- src/gulp/markdown-post-file.ts
+++ src/gulp/markdown-post-file.ts
@@ -1,6 +1,5 @@
 import marked from 'marked';
 import readingTime from 'reading-time';
-import Vinyl from 'Vinyl';
 import { ContentMeta } from './content-meta';
 import { EjsTemplate } from './ejs-template';
 
@@ -9,7 +8,7 @@ export class MarkdownPostFile {
   private content: string;
   private markdown: string;
 
-  constructor(private file: Vinyl) {
+  constructor(private file) {
     const contentMeta = new ContentMeta(this.file.contents.toString());
     this.metadata = contentMeta.metadata();
     this.markdown = contentMeta.content();
